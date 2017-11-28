63431ff96e9791327444c05122c67f0c47a90759
Patrick Miner <patrickminer318@gmail.com>
2017-11-27 23:48:14 -0800

    Get list of books to render on page

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Harry Potter' },
+    { title: 'Game of Thrones' },
+    { title: 'How to Win Friends and Influence People' },
+    { title: 'No Bullshit' },
+  ]
+}
