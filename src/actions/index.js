c1466d7d01d963dd869c6049fa4d8ef69ff20319
Patrick Miner <patrickminer318@gmail.com>
2017-12-03 19:06:04 -0800

    Working API request and back up to speed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export function selectBook(book) {
   // selectBook is is an actionCreator that needs to return an action: an object with a 'type' property
   return {
@@ -6,3 +7,21 @@ export function selectBook(book) {
   }
 }
 
+=======
+import axios from 'axios';
+
+const API_KEY = 'a7c6330c6bae9e3cb15a782c927abe06';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url); // returns a promise
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request, // passing promise 
+  }
+}
+>>>>>>> Working API request and back up to speed
--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
+<<<<<<< HEAD
 import BookList from '../containers/book-list';
+=======
+import SearchBar from '../containers/search_bar';
+>>>>>>> Working API request and back up to speed
 
 export default class App extends Component {
   render() {
     return (
+<<<<<<< HEAD
       <div>
         <BookList />
       </div>
+=======
+      <div><SearchBar /></div>
+>>>>>>> Working API request and back up to speed
     );
   }
 }
