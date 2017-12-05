150aaf75ad09a869c3619330435bf5cd25e01da2
Patrick Miner <patrickminer318@gmail.com>
2017-12-04 21:27:57 -0800

    Completed app

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -6,7 +6,7 @@ export default function(state = [], action) {
       // we use concat instead of push because we want to return a completely new instance of application state (instead of manipulating it)
       // return state.concat([ action.payload.data ]);
       console.log('This is state: ', state);
-      return [ action.payload.data, ...state ] // returns a new array with action.payload.data and all the destructured elements inside array 'state'
+      return [ action.payload.data, ...state ] // returns a new array with action.payload.data and all the 'destructured' elements inside array 'state'
   }
   return state;
 }
